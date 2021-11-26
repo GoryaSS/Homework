5d3da8a3932577c2ff163e030d14c103e6d29878
Ihor <61710130+GoryaSS@users.noreply.github.com>
2021-11-26 14:25:30 +0200

    add "normal" section with task 1 - Sum of all proper divisors

---
+++ #28/28.normal.js
@@ -0,0 +1,23 @@
+// -------------- 1 - sum of the proper divisors -----------------
+
+function sumProperDivisors(num) {
+// Final result of summation of divisors
+  let result = 0;
+// find all divisors which divides 'num'
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+// if 'i' is divisor of 'num'
+      if (num % i == 0) {
+// if both divisors are same then add
+// it only once else add both
+        if (i==(num / i))
+            result += i;
+        else
+            result += (i + num / i);
+      }
+  }
+// Add 1 to the result as 1 is also a divisor
+  return (result + 1);  
+}
+
+let num = 6;
+console.log((sumProperDivisors(num) == num) ? "true": "false");
