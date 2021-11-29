5de985258e9cba1b5edcf027f98036b6a418c216
Ihor <61710130+GoryaSS@users.noreply.github.com>
2021-11-29 15:15:35 +0200

    Update task-1 and add task-2 in normal section

--- #28/28.normal.js
+++ #28/28.normal.js
@@ -1,23 +1,64 @@
-// -------------- 1 - sum of the proper divisors -----------------
+// --------------task 1 - sum of the proper divisors -----------------
+let num = 5;
 
-function sumProperDivisors(num) {
-// Final result of summation of divisors
-  let result = 0;
-// find all divisors which divides 'num'
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-// if 'i' is divisor of 'num'
-      if (num % i == 0) {
-// if both divisors are same then add
-// it only once else add both
-        if (i==(num / i))
-            result += i;
-        else
-            result += (i + num / i);
-      }
+function sumPerfectNumbers() {
+   let result = 0;
+  for (let i = 0; i < num; i++) {
+    if (num % i == 0) {
+        result += i;
+    }
   }
-// Add 1 to the result as 1 is also a divisor
-  return (result + 1);  
+  if (result == num) {
+   console.log(num + " is a perfect number.");
+  } else {
+   console.log(num + " is not a perfect number.");
+  }
+}
+sumPerfectNumbers(num);
+
+// Old-------------- Old------------ Old------------ Old------------ Old------------ Old
+
+// function sumPerfectNumbers(num) {
+// // Final result of summation of divisors
+//   let result = 0;
+// // find all divisors which divides 'num'
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+// // if 'i' is divisor of 'num'
+//       if (num % i == 0) {
+// // if both divisors are same then add
+// // it only once else add both
+//         if (i==(num / i))
+//             result += i;
+//         else
+//             result += (i + num / i);
+//       }
+//   }
+// // Add 1 to the result as 1 is also a divisor
+//   return (result + 1);  
+// }
+
+// let num = 6;
+// console.log((sumPerfectNumbers(num) == num) ? "true": "false");
+
+
+// // -------------- task 2 - The range of perfect numbers
+
+function sumPerfectNumbers(num) {
+   let result = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i == 0) {
+        result += i;
+    }
+  }
+  if (result == num) {
+   console.log(`The perfect number is ${result}`);
+  } 
 }
 
-let num = 6;
-console.log((sumProperDivisors(num) == num) ? "true": "false");
+function rengeNumbers(numRangeStart, numRangeEnd) {
+  for (let i = numRangeStart; i <= numRangeEnd; i++) {
+    sumPerfectNumbers(i);
+  }
+}
+rengeNumbers(2, 512);
+
