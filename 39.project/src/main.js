0f9f884d58916f41a8d252418d40eab236238f7e
IhorBor <borisevitchigor@gmail.com>
2022-01-10 22:00:59 +0200

    solwe problom with menue & fetch

--- 39.project/src/main.js
+++ 39.project/src/main.js
@@ -5,39 +5,40 @@ import { createContacts } from './script/contacts.js'
 
 function createDoc() {  
   const headerCourse = document.querySelector('#header-course');
-  const mainCourse = document.querySelector('#main-course')
-  
-  const footerCourse = document.querySelector('#footer-course');
-  footerCourse.textContent = 'JS Course Beetroot';  
-  
-  const loginCourse = document.querySelector('.login-course');
-  loginCourse.addEventListener('click', (e) =>
-    handlerPlaceHolder(e, placeHolder, autUser())
-  );
+  const mainCourse = document.querySelector('#main-course');
 
-  const contentCourse = document.querySelector('.content-course');
-  contentCourse.addEventListener('click', async (e) => {
-    const dataContent = await createContent();
-    const mainCourse = document.querySelector('#main-course');
-    handlerData(e, mainCourse.firstChild, dataContent);
+  // кнопка хоум - переносить на домашню сторінку
+  const homeCourse = document.querySelector('.course-home');
+  homeCourse.onclick = () => window.location.reload();
+
+  // клік по лого - переносить на домашню сторінку
+  const logoCourse = document.querySelector('.logo-course');
+  logoCourse.onclick = () => window.location.reload();
+
+  const loginCourse = document.querySelector('.login-course');
+  loginCourse.addEventListener('click', (e) => {
+    e.preventDefault();
+    handlerData(e, mainCourse.firstElementChild, autUser());
   });
 
   const courseContacts = document.querySelector('.contacts-course');
   courseContacts.addEventListener('click', async (e) => {
     const dataContent = await createContacts();
-    const mainCourse = document.querySelector('#main-course');
-    handlerData(e, mainCourse.firstChild, dataContent);
+    e.preventDefault();
+    handlerData(e, mainCourse.firstElementChild, dataContent); 
+  });
+
+  const contentCourse = document.querySelector('.content-course');
+  contentCourse.addEventListener('click', async (e) => {
+    const dataContent = await createContent();
+    e.preventDefault();
+    handlerData(e, mainCourse.firstElementChild, dataContent);
   });
   
-  // кнопка абаут - скидає локал сторедж і переносить на домашню сторінку
+  // кнопка reset - скидає локал сторедж і переносить на домашню сторінку
   const aboutCourse = document.querySelector('#about-course');
   aboutCourse.onclick = () => localStorage.clear(window.location.reload());
-  // кнопка хоум - переносить на домашню сторінку
-  const homeCourse = document.querySelector('.course-home');
-  homeCourse.onclick = () => window.location.reload();
-  // клік по лого - переносить на домашню сторінку
-  const logoCourse = document.querySelector('.logo-course');
-  logoCourse.onclick = () => window.location.reload();
+
 
   const placeHolder = document.querySelector('.place-course');
   let userName = localStorage.getItem('nameUser');
@@ -50,6 +51,9 @@ function createDoc() {
   placeHolder.addEventListener('click', (welkome) =>
     handlerPlaceHolder(welkome, placeHolder, autUser())
   );
+
+  const footerCourse = document.querySelector('#footer-course');
+  footerCourse.textContent = 'JS Course Beetroot';  
 }
 
 function handlerPlaceHolder(event, oldNode, newNode) {
